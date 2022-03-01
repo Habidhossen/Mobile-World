59ea179a890fc27423e33ccec66f8d36fd33254d
Habidhossen <habidhossen2@gmail.com>
2022-03-01 15:24:47 +0600

    code added

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ const displayPhone = (phones) => {
   const resultContainer = document.getElementById("result-container");
   resultContainer.innerHTML = "";
   phones.forEach((phone) => {
-    console.log(phone);
+    // console.log(phone);
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
@@ -32,10 +32,61 @@ const displayPhone = (phones) => {
             </div>
             <h5 class="phone-title">${phone.phone_name}</h5>
             <p class="phone-text">Brand: ${phone.brand}</p>
-            <button id="" class="details-btn w-100">Details</button>
-        </div>
+
+                <button
+                  onclick="loadPhoneDetails('${phone.slug}')"
+                  class="details-btn w-100"
+                  data-bs-toggle="modal"
+                  data-bs-target="#see-details">Details
+                </button>
+            </div>
     
     `;
     resultContainer.appendChild(div);
   });
+};
+
+/* load phone details */
+const loadPhoneDetails = (phoneId) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayPhoneDetails(data.data));
+};
+
+/* display phone details */
+const displayPhoneDetails = (phone) => {
+  console.log(phone.mainFeatures);
+  const modalBody = document.getElementById("modal-body");
+  modalBody.innerHTML = "";
+  const div = document.createElement("div");
+  div.classList.add("div");
+  div.innerHTML = `
+  
+  
+                <div class="text-center">
+                    <img
+                      src="${phone.image}"
+                      class="w-50"
+                      alt="..."
+                    />
+                  </div>
+                  <h5 class="modal-phone-title">${phone.name}</h5>
+                  <p>Release date: 22 february 2022</p>
+                  <p>Brand: ${phone.brand}</p>
+
+                  <h5 class="modal-subtitle">Main features</h5>
+                  <p>Storage: ${phone.mainFeatures.storage}</p>
+                  <p>Display Size: ${phone.mainFeatures.displaySize}</p>
+                  <p>Memory: ${phone.mainFeatures.memory}</p>
+
+                  <h5 class="modal-subtitle">Others</h5>
+                  <p>Release date: 22 february 2022</p>
+                  <p>Brand: Samsung</p>
+  
+  
+  
+  
+  `;
+  modalBody.appendChild(div);
 };
